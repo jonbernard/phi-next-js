184e6571c98919a6c6ad4080ff0dca0e1c924523
Jon Bernard <jon@jonbernard.net>
2024-01-27 23:26:26 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,8 @@ export default async function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        <code className="font-mono font-bold">
-          {JSON.stringify((data as { id: string }[]).map(({ id }) => id))}
-        </code>
+        <code className="font-mono font-bold">{process.env.STRAPI_TOKEN}</code>
+        <code className="font-mono font-bold">{JSON.stringify(data)}</code>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
